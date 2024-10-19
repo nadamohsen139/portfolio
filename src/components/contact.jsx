675fd714bbe78ce715380c36ed1c10717d544d53
nadamohsen139 <mohsennada139@gmail.com>
2024-10-19 19:18:12 +0300

    adding resume

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -2,13 +2,23 @@ const Contact = () => {
   return (
     <div className="contact pb-20 text-center" id="contact">
       <h2 className="my-20 text-center text-4xl font-semibold">Contact Me</h2>
-      <div className="font-semibold mb-6 lg:text-2xl">
+      <div className="font-semibold mb-10 lg:text-2xl">
         You Can Contact Me Directly on{" "}
         <a
           href="mailto:mohsennada139@gmail.com"
           className="font-bold text-pink-400"
         >
           My Gmail Acc
+        </a>
+      </div>
+      <div className="font-semibold mb-6 lg:text-2xl">
+        You Can Download My Resume from{" "}
+        <a
+          href="/src/assets/Nada-Mohsen-Resume.pdf"
+          download
+          className="font-bold text-pink-400"
+        >
+          Here
         </a>
       </div>
     </div>
